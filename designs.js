82f29d30936a7f7eb43877014deae41dcad45858
fernando-ts <torresinc7@mesoamericana.edu.gt>
2018-08-07 17:36:20 +0200

    Added retrieval of color, height and width values
    3 variables that retrieve values from the DOM where added.

--- designs.js
+++ designs.js
@@ -1,5 +1,12 @@
 // Select color input
+const chosenColor = document.getElementById('colorPicker').value;
+console.log(chosenColor);
 // Select size input
+const heightSize = document.getElementById('inputHeight').value;
+const widthSize = document.getElementById('inputWidth').value;
+console.log(heightSize);
+console.log(widthSize);
+
 
 // When size is submitted by the user, call makeGrid()
 
