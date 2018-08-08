80fd2ce7120235af2d85e1013925a1b9a4661632
fernando-ts <torresinc7@mesoamericana.edu.gt>
2018-08-08 02:53:52 +0200

    Added the grid maker event
    The make grid function was created and then it is called with the
    make grid call in the event listener for submmiting

--- designs.js
+++ designs.js
@@ -1,17 +1,30 @@
 // Select color input
 const chosenColor = document.getElementById('colorPicker').value;
-console.log(chosenColor);
-// Select size input
-const heightSize = document.getElementById('inputHeight').value;
-const widthSize = document.getElementById('inputWidth').value;
-console.log(heightSize);
-console.log(widthSize);
 
+//Select the skeleton's table to be constructed
+const theTbl = document.getElementById('pixelCanvas');
+
+//Select the submit action from the form
+const submitReady = document.getElementById('sizePicker');
 
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
-
-// Your code goes here!
-
+    // Select size input
+    const heightSize = document.getElementById('inputHeight').value;
+    const widthSize = document.getElementById('inputWidth').value;
+    for (let i = 0; i < heightSize; i++) {
+        let newRow = document.createElement('tr');
+        theTbl.appendChild(newRow);
+        for (let j = 0; j <widthSize; j++) {
+            let newCell = document.createElement('td');
+            newRow.appendChild(newCell);            
+        }  
+    }
 }
+
+//Add event listener to the submit-form-input 
+submitReady.addEventListener('submit', function (e) {
+    e.preventDefault();
+    makeGrid();
+})
